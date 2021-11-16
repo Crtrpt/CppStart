bbc28befa44d2b29344ba93b34a9b191c85dda3f
jzx <945627077@qq.com>
2021-11-16 11:17:00 +0800

    '修改宏日志'

--- CppStart.cpp
+++ CppStart.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include "Server.h"
 #include "Client.h"
+#include "log.h"
+using namespace std;
+
 extern "C" int foo(int num, int power);
 
 int power2( int num, int power );
 
-char format[] = "%s ============== \n";
-char hello[] = "Hello";
 
+inline void logInfo(string v){
+    cout << "Value of __LINE__ : " << __LINE__ << endl;
+    cout << "Value of __FILE__ : " << __FILE__ << endl;
+    cout << "Value of __DATE__ : " << __DATE__ << endl;
+    cout << "Value of __TIME__ : " << __TIME__ << endl;
+}
 
 int main() {
     std::cout <<  foo(2, 2) << std::endl;
     std::cout << power2(2,2) << std::endl;
-
+    LOGINFO("micro log ");
     auto *u= new Server();
     std::cout << u->getName() << std::endl;
     std::cout << u->getAge() << std::endl;
---
+++ log.h
@@ -0,0 +1,12 @@
+//
+// Created by Admin on 2021/11/16.
+//
+
+#ifndef CPPSTART_LOG_H
+#define CPPSTART_LOG_H
+
+
+#define LOGINFO(s) ( cout <<"["<<__FILE__<< ":"<<__LINE__  <<"] [" << __DATE__  <<" "<< __TIME__<<"] " << s << endl)
+
+
+#endif //CPPSTART_LOG_H
